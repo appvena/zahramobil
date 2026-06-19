2119110a51c971aa650508d2d9382f0f8416b184
appvena <admin.vena@gmail.com>
2026-06-19 14:12:42 +0700

    Include Poppins font in RootLayout
    Added Google Fonts preconnect and stylesheet links.

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
       <body style={{ margin: 0, padding: 0, overflowX: "hidden", maxWidth: "100vw" }}>{children}</body>
     </html>
   );
